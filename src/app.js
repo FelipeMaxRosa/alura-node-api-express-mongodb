10d55a79fd2ca6808b987ed81f1f971aee7e6bdc
Felipe Rosa <felipe.rosa@domo.com>
2023-04-01 11:57:55 +0100

    chore: cleaning code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import db from './config/dbConnect.js';
-import books from './models/Book.js';
 import routes from './routes/index.js';
 
 db.on("error", () => console.log("DB connection error"));
@@ -12,32 +11,5 @@ db.once("open", () => {
 const app = express();
 app.use(express.json());
 routes(app);
-
-
-app.get('/books/:id', (req, res) => {
-  const bookIndex = getBookIndex(req.params.id);
-
-  res.json(books[bookIndex]);
-});
-
-app.put('/books/:id', (req, res) => {
-  const bookIndex = getBookIndex(req.params.id);
-  books[bookIndex].title = req.body.title;
-
-  res.json(books);
-});
-
-app.delete('/books/:id', (req, res) => {
-  const { id } = req.params;
-  const bookIndex = getBookIndex(id);
-
-  books.splice(bookIndex, 1);
-
-  res.send(`Book ${id} has been removed successfully!`);
-});
-
-function getBookIndex(id) {
-  return books.findIndex((book) => book.id == id);
-}
 
 export default app;
